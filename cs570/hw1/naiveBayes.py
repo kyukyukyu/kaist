14f121cd2ab5765691bf82833628390fa053664c
Sanggyu Nam <pokeplus@gmail.com>
2016-03-15 20:17:13 +0900

    Fix whitespace issues, done by Vim

--- cs570/hw1/naiveBayes.py
+++ cs570/hw1/naiveBayes.py
@@ -5,7 +5,7 @@ import math
 class NaiveBayesClassifier(classificationMethod.ClassificationMethod):
   """
   See the project description for the specifications of the Naive Bayes classifier.
-  
+
   Note that the variable 'datum' in this code refers to a counter of features
   (not to a raw samples.Datum).
   """
@@ -30,38 +30,38 @@ class NaiveBayesClassifier(classificationMethod.ClassificationMethod):
   def train(self, trainingData, trainingLabels, validationData, validationLabels):
     """
     Outside shell to call your method. Do not modify this method.
-    """  
-      
+    """
+
     self.features = trainingData[0].keys() # this could be useful for your code later...
-    
+
     if (self.automaticTuning):
         kgrid = [0.001, 0.01, 0.05, 0.1, 0.5, 1, 5, 10, 20, 50]
     else:
         kgrid = [self.k]
-        
+
     self.trainAndTune(trainingData, trainingLabels, validationData, validationLabels, kgrid)
-      
+
   def trainAndTune(self, trainingData, trainingLabels, validationData, validationLabels, kgrid):
     """
     Trains the classifier by collecting counts over the training data, and
     stores the Laplace smoothed estimates so that they can be used to classify.
-    Evaluate each value of k in kgrid to choose the smoothing parameter 
+    Evaluate each value of k in kgrid to choose the smoothing parameter
     that gives the best accuracy on the held-out validationData.
-    
+
     trainingData and validationData are lists of feature Counters.  The corresponding
     label lists contain the correct label for each datum.
-    
-    To get the list of all possible features or labels, use self.features and 
+
+    To get the list of all possible features or labels, use self.features and
     self.legalLabels.
     """
 
     "*** YOUR CODE HERE ***"
     util.raiseNotDefined()
-        
+
   def classify(self, testData):
     """
     Classify the data based on the posterior distribution over labels.
-    
+
     You shouldn't modify this method.
     """
     guesses = []
@@ -71,17 +71,17 @@ class NaiveBayesClassifier(classificationMethod.ClassificationMethod):
       guesses.append(posterior.argMax())
       self.posteriors.append(posterior)
     return guesses
-      
+
   def calculateLogJointProbabilities(self, datum):
     """
     Returns the log-joint distribution over legal labels and the datum.
-    Each log-probability should be stored in the log-joint counter, e.g.    
+    Each log-probability should be stored in the log-joint counter, e.g.
     logJoint[3] = <Estimate of log( P(Label = 3, datum) )>
     """
     logJoint = util.Counter()
-    
+
     "*** YOUR CODE HERE ***"
     util.raiseNotDefined()
-    
+
     return logJoint
-  
+
