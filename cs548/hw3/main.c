cf440d63e3f4db24a8439e61836fb19debd733a9
Sanggyu Nam <pokeplus@gmail.com>
2016-10-10 22:10:47 +0900

    CS548-HW3: Add printing the Hamming distances

--- cs548/hw3/main.c
+++ cs548/hw3/main.c
@@ -7,12 +7,14 @@
 
 int print_hash(const char *algorithm, unsigned int pt, unsigned char *ht,
         int xor);
+int print_hdist(unsigned char *ht1, unsigned char *ht2);
 
 int main(int argc, const char* argv[]) {
     unsigned int yyyymmdd;
     unsigned int yyyymmdd_1;
     int in;
-    unsigned char h[HASH_LENGTH];
+    unsigned char h1[HASH_LENGTH];
+    unsigned char h2[HASH_LENGTH];
 
     fputs("YYYYMMDD: ", stdout);
     in = scanf("%8u", &yyyymmdd);
@@ -26,23 +28,27 @@ int main(int argc, const char* argv[]) {
 
     yyyymmdd_1 = yyyymmdd ^ 0x01;
 
-    SHA256_Encrpyt((BYTE *) &yyyymmdd, sizeof(yyyymmdd), h);
-    print_hash("SHA256", yyyymmdd, h, 0);
+    SHA256_Encrpyt((BYTE *) &yyyymmdd, sizeof(yyyymmdd), h1);
+    print_hash("SHA256", yyyymmdd, h1, 0);
 
-    SHA256_Encrpyt((BYTE *) &yyyymmdd_1, sizeof(yyyymmdd_1), h);
-    print_hash("SHA256", yyyymmdd, h, 1);
+    SHA256_Encrpyt((BYTE *) &yyyymmdd_1, sizeof(yyyymmdd_1), h2);
+    print_hash("SHA256", yyyymmdd, h2, 1);
+
+    print_hdist(h1, h2);
 
     lsh_digest(LSH_MAKE_TYPE(0, HASH_LENGTH),
             (lsh_u8 *) &yyyymmdd,
             8 * sizeof(yyyymmdd),
-            h);
-    print_hash("LSH", yyyymmdd, h, 0);
+            h1);
+    print_hash("LSH", yyyymmdd, h1, 0);
 
     lsh_digest(LSH_MAKE_TYPE(0, HASH_LENGTH),
             (lsh_u8 *) &yyyymmdd_1,
             8 * sizeof(yyyymmdd_1),
-            h);
-    print_hash("LSH", yyyymmdd, h, 1);
+            h2);
+    print_hash("LSH", yyyymmdd, h2, 1);
+
+    print_hdist(h1, h2);
 
     return 0;
 }
@@ -63,4 +69,20 @@ int print_hash(const char *algorithm, unsigned int pt, unsigned char *ht,
     }
     *sp = '\0';
     return printf("%s(%u%s): %s\n", algorithm, pt, as, s);
+}
+
+int print_hdist(unsigned char *ht1, unsigned char *ht2) {
+    int i = HASH_LENGTH / (8 * sizeof(unsigned long long));
+    int hdist = 0;
+    unsigned long long *p1 = (unsigned long long *) ht1;
+    unsigned long long *p2 = (unsigned long long *) ht2;
+    while (i > 0) {
+        // GCC builtin which calculates the Hamming distance between two
+        // values.
+        hdist += __builtin_popcountll(*p1 ^ *p2);
+        ++p1;
+        ++p2;
+        --i;
+    }
+    return printf("Hamming distance: %d\n", hdist);
 }
